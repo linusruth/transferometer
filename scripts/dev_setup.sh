e96e2c6767ad47bbb1fd8f19cd2bba3203d049e6
Linus Ruth <linus128k@yahoo.com>
2018-05-28 14:17:52 -0700

    feat: verify vboxmanage exists, functionize repetitive tasks

--- scripts/dev_setup.sh
+++ scripts/dev_setup.sh
@@ -15,7 +15,7 @@ exact_match_in_list() {
 }
 
 command_exists() {
-  (test -n "$(command -v ${1})" && return 0) || return 1
+  test -n "$(command -v ${1})"
 }
 
 which_command() {
@@ -27,19 +27,32 @@ which_command() {
   done
 }
 
+fail() {
+  printf "\nError: ${1}\n\n"
+  exit 1
+}
+
+verify_command_in_path() {
+  printf "Verifying that '${1}' is in command path... "
+
+  if command_exists "${1}"; then
+    printf "true\n"
+  else
+    printf "false\n"
+    fail "Unable to locate '${1}' in command path."
+  fi
+}
+
 determine_host_os() {
   printf "Determining host operating system... "
   local ACCEPTED_VALUES="CYGWIN Darwin Linux MINGW"
 
   HOST_OS="$(uname -s)"
+  printf "${HOST_OS}\n"
 
-  if partial_match_in_list "${HOST_OS}" "${ACCEPTED_VALUES}"; then
-    printf "${HOST_OS}\n"
-  else
-    printf "error\n"
-    printf "Host operating system '${HOST_OS}' is not supported.\n"
-    printf "Supported operating systems are: ${ACCEPTED_VALUES}\n"
-    exit 1
+  if ! partial_match_in_list "${HOST_OS}" "${ACCEPTED_VALUES}"; then
+    fail "Host operating system '${HOST_OS}' is not supported.\n" \
+      "Supported operating systems are: ${ACCEPTED_VALUES}"
   fi
 }
 
@@ -50,14 +63,11 @@ determine_host_architecture() {
   local ACCEPTED_VALUES="i386 i486 i586 i686 x86_64"
 
   HOST_ARCHITECTURE="$(uname -m)"
+  printf "${HOST_ARCHITECTURE}\n"
 
-  if exact_match_in_list "${HOST_ARCHITECTURE}" "${ACCEPTED_VALUES}"; then
-    printf "${HOST_ARCHITECTURE}\n"
-  else
-    printf "error\n"
-    printf "Host architecture '${HOST_ARCHITECTURE}' is not supported.\n"
-    printf "Supported architectures are: ${ACCEPTED_VALUES}\n"
-    exit 1
+  if ! exact_match_in_list "${HOST_ARCHITECTURE}" "${ACCEPTED_VALUES}"; then
+    fail "Host architecture '${HOST_ARCHITECTURE}' is not supported.\n" \
+      "Supported architectures are: ${ACCEPTED_VALUES}"
   fi
 }
 
@@ -75,13 +85,10 @@ determine_host_virtualization_extensions() {
     HOST_EXTENSIONS="$(powershell -c "(GWMI Win32_Processor).VirtualizationFirmwareEnabled")"
     HOST_EXTENSIONS="$(printf "${HOST_EXTENSIONS}" | tr "[:upper:]" "[:lower:]")"
   fi
+  printf "${HOST_EXTENSIONS}\n"
 
-  if exact_match_in_list "${HOST_EXTENSIONS}" "${ACCEPTED_VALUES}"; then
-    printf "${HOST_EXTENSIONS}\n"
-  else
-    printf "error\n"
-    printf "Unable to determine if host CPU has virtualization extensions.\n"
-    exit 1
+  if ! exact_match_in_list "${HOST_EXTENSIONS}" "${ACCEPTED_VALUES}"; then
+    fail "Unable to determine if host CPU has virtualization extensions."
   fi
 }
 
@@ -96,13 +103,10 @@ determine_openwrt_architecture() {
   else
     OPENWRT_ARCHITECTURE="x86-generic"
   fi
+  printf "${OPENWRT_ARCHITECTURE}\n"
 
-  if exact_match_in_list "${OPENWRT_ARCHITECTURE}" "${ACCEPTED_VALUES}"; then
-    printf "${OPENWRT_ARCHITECTURE}\n"
-  else
-    printf "error\n"
-    printf "Unable to determine OpenWRT architecture.\n"
-    exit 1
+  if ! exact_match_in_list "${OPENWRT_ARCHITECTURE}" "${ACCEPTED_VALUES}"; then
+    fail "Unable to determine OpenWRT architecture."
   fi
 }
 
@@ -121,14 +125,11 @@ determine_download_utility() {
   local ACCEPTED_VALUES="curl wget"
 
   DOWNLOAD_UTILITY="$(which_command "${ACCEPTED_VALUES}")"
+  printf "${DOWNLOAD_UTILITY}\n"
 
-  if exact_match_in_list "${DOWNLOAD_UTILITY}" "${ACCEPTED_VALUES}"; then
-    printf "${DOWNLOAD_UTILITY}\n"
-  else
-    printf "error\n"
-    printf "Unable to locate compatible download utility.\n"
-    printf "Supported download utilitities are: ${ACCEPTED_VALUES}\n"
-    exit 1
+  if ! exact_match_in_list "${DOWNLOAD_UTILITY}" "${ACCEPTED_VALUES}"; then
+    fail "Unable to locate compatible download utility.\n" \
+      "Supported download utilitities are: ${ACCEPTED_VALUES}"
   fi
 }
 
@@ -151,14 +152,11 @@ determine_extraction_utility() {
   local ACCEPTED_VALUES="gunzip gzip"
 
   EXTRACTION_UTILITY="$(which_command "${ACCEPTED_VALUES}")"
+  printf "${EXTRACTION_UTILITY}\n"
 
-  if exact_match_in_list "${EXTRACTION_UTILITY}" "${ACCEPTED_VALUES}"; then
-    printf "${EXTRACTION_UTILITY}\n"
-  else
-    printf "error\n"
-    printf "Unable to locate compatible extraction utility.\n"
-    printf "Supported extraction utilities are: ${ACCEPTED_VALUES}\n"
-    exit 1
+  if ! exact_match_in_list "${EXTRACTION_UTILITY}" "${ACCEPTED_VALUES}"; then
+    fail "Unable to locate compatible extraction utility.\n" \
+      "Supported extraction utilities are: ${ACCEPTED_VALUES}"
   fi
 }
 
@@ -178,6 +176,7 @@ extract_firmware_image() {
 
 convert_firmware_image() {
   extract_firmware_image
+  verify_command_in_path "VBoxManage"
 
   printf "Converting firmware image to VirtualBox Disk Image (VDI)... "
   FIRMWARE_IMAGE="${FIRMWARE_PACKAGE//\.gz/}"
@@ -198,13 +197,10 @@ determine_vm_os_type() {
   elif test ${OPENWRT_ARCHITECTURE} = "x86-64"; then
     VM_OS_TYPE="Linux26_64"
   fi
+  printf "${VM_OS_TYPE}\n"
 
-  if exact_match_in_list "${VM_OS_TYPE}" "${ACCEPTED_VALUES}"; then
-    printf "${VM_OS_TYPE}\n"
-  else
-    printf "error\n"
-    printf "Unable to determine virtual machine OS type.\n"
-    exit 1
+  if ! exact_match_in_list "${VM_OS_TYPE}" "${ACCEPTED_VALUES}"; then
+    fail "Unable to determine virtual machine OS type."
   fi
 }
 
@@ -232,13 +228,10 @@ determine_vm_longmode() {
   else
     VM_LONGMODE="off"
   fi
+  printf "${VM_LONGMODE}\n"
 
-  if exact_match_in_list "${VM_LONGMODE}" "${ACCEPTED_VALUES}"; then
-    printf "${VM_LONGMODE}\n"
-  else
-    printf "error\n"
-    printf "Unable to determine virtual machine 'longmode' setting.\n"
-    exit 1
+  if ! exact_match_in_list "${VM_LONGMODE}" "${ACCEPTED_VALUES}"; then
+    fail "Unable to determine virtual machine 'longmode' setting."
   fi
 }
 
